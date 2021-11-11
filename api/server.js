ab0a8d89dcc2a3d5e5d8bbdb6613d1de56b34ec9
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-11 13:50:07 -0800

    added error handling middleware to server.js

--- api/server.js
+++ api/server.js
@@ -2,4 +2,10 @@ const express = require("express")
 
 const server = express()
 
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message || "Oh d-d-d-dear!"
+  })
+})
+
 module.exports = server
