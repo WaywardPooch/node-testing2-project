630003eb696eaeb636bd7b7f252aef6eff7f8802
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-11 15:20:05 -0800

    added tests for posting new tasks to db

--- api/server.spec.js
+++ api/server.spec.js
@@ -24,10 +24,53 @@ describe("[GET] /api/tasks", () => {
 })
 
 describe("[GET] /api/tasks/:id", () => {
-  it("responds with status 200", async () => {
+  it("responds with status 200 when ID is valid", async () => {
     const expected = 200
     const res = await request(server).get("/api/tasks/1")
     const actual = res.status
     expect(actual).toBe(expected)
+  })
+  it("responds with status 404 when ID is invalid", async () => {
+    const expected = 404
+    const res = await request(server).get("/api/tasks/7")
+    const actual = res.status
+    expect(actual).toBe(expected)
+  })
+})
+
+describe("[POST] /api/tasks", () => {
+  let newTask
+  beforeEach(() => {
+    newTask = { task_name: "find the key" }
+  })
+
+  it("responds with status 201 when request is successful", async () => {
+    const expected = 201
+    const res = await request(server).post("/api/tasks", newTask)
+    const actual = res.status
+    expect(actual).toBe(expected)
+  })
+  it("can add valid task to database", async () => {
+    const expected = {
+      task_id: 4,
+      task_name: "find the key",
+      task_description: null,
+      task_completed: false
+    }
+    await request(server).post("/api/tasks", newTask)
+    const res = await request(server).get("/api/tasks/4")
+    const actual = res.body
+    expect(actual).toEqual(expected)
+  })
+  it("returns the newly added task when successful", async () => {
+    const expected = {
+      task_id: 4,
+      task_name: "find the key",
+      task_description: null,
+      task_completed: false
+    }
+    const res = await request(server).post("/api/tasks", newTask)
+    const actual = res.body
+    expect(actual).toEqual(expected)
   })
 })
