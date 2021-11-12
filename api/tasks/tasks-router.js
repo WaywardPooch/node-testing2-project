95704f96510ec88c0c0a52159ea68b9514d8f907
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-11 16:11:06 -0800

    refactored get /:id, added POST new task endpoint

--- api/tasks/tasks-router.js
+++ api/tasks/tasks-router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router()
 const Task = require("./tasks-model")
-const { validateTaskId } = require("./tasks-middleware")
+const {
+  validateTaskId,
+  validateTaskPayload
+} = require("./tasks-middleware")
 
 router.get("/",
   async (req, res, next) => {
@@ -17,9 +20,21 @@ router.get("/:id",
   validateTaskId,
   async (req, res, next) => {
     try {
-      const { id } = req.params
       const task = req.custom_task
       res.status(200).json(task)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
+
+router.post("/",
+  validateTaskPayload,
+  async (req, res, next) => {
+    try {
+      const task = req.custom_task
+      const newTask = await Task.add(task)
+      res.status(201).json(newTask)
     } catch (err) {
       next(err)
     }
